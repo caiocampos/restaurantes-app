0e04786e08189f4bed4d335d6698fca0c5b78003
Caio <Caio@Caio-PC>
2018-04-14 15:03:48 -0300

    Ajuste

--- src/login/login.ts
+++ src/login/login.ts
@@ -1,8 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { LoginModule } from './app/views/login/login.module';
-import { environment } from './environments/environment';
+import { LoginModule } from './../app/views/login/login.module';
+import { environment } from './../environments/environment';
 
 if (environment.production) {
   enableProdMode();
