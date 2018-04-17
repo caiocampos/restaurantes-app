0256457427f1cad804b7ee2576fecf979572351b
caiocampos <caio_campos1204@hotmail.com>
2018-04-16 22:00:11 -0300

    Ajustes para login

---
+++ src/app/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+  title = 'Demo';
+  greeting = {};
+
+  constructor(private app: AppService, private http: HttpClient) {
+  }
+
+  authenticated() { return this.app.authenticated; }
+
+}
