c3dfcfe0dd2821956ea6360b447fa09bfaf0475b
caiocampos <caio_campos1204@hotmail.com>
2018-04-16 22:00:11 -0300

    Ajustes para login

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { LoginModule } from './app/views/login/login.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(LoginModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
