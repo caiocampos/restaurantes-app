b271c07d527e16e73f7c61cccfe7b5ccc2302fd4
Caio Campos <caio.campos1204@gmail.com>
2020-11-19 18:09:35 -0300

    atualização de Angular

--- src/app/component/dynaview/dynaview.component.spec.ts
+++ src/app/component/dynaview/dynaview.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DynaviewComponent } from './dynaview.component';
 
@@ -6,7 +6,7 @@ describe('DynaviewComponent', () => {
   let component: DynaviewComponent;
   let fixture: ComponentFixture<DynaviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DynaviewComponent ]
     })
