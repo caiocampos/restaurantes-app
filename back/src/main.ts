7b5759ad1f66d74a0e2bd50946ca0b43613ef1f1
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2026-07-08 10:11:25 -0300

    change prefix

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 
+const prefix = "restaurantes-api";
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -14,14 +16,16 @@ async function bootstrap() {
     }),
   );
 
-  app.setGlobalPrefix("api");
+  app.setGlobalPrefix(prefix);
   app.enableCors();
 
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
 
   // eslint-disable-next-line no-console
-  console.log(`🍽️  Restaurantes API rodando em http://localhost:${port}/api`);
+  console.log(
+    `🍽️  Restaurantes API rodando em http://localhost:${port}/${prefix}`,
+  );
 }
 
 bootstrap();
